e0b3b87d14d11347bcd4424fda327fbe9d8cd6af
mehunk <mehunk@163.com>
2016-02-02 18:24:46 +0800

    Change: Modify port.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	'port': process.env.PORT || 8080,
+	'port': process.env.PORT || 8085,
     'wechat': {
         'appid': 'wxafeda6528eb6a895',
         'appsecret': 'ed4945f780cf8cdf1ae0ed2be156779e'
